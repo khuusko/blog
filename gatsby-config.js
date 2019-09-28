67f35389221584eb2e757d7b4378cc26511fad3f
Kaisa Huusko <kaisah.huusko@gmail.com>
2019-09-28 12:28:14 +0300

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    author: `Kaisa Huusko`,
+    description: `A blogl`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `tietokonna`,
     },
   },
   plugins: [
